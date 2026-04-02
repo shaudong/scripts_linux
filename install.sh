82ff15e7941bf8902fbedeffb69499cd5ec57314
CHC <ch_chiang@arcadyan.com>
2026-04-03 02:26:25 +0800

    refine install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,17 @@
-cp /usr/bin/docker_init usr/bin/
-cp /usr/bin/hudson_exec usr/bin/
-cp /etc/systemd/system/hudson-slave.service etc/systemd/system/
-cp /etc/systemd/system/harbor.service etc/systemd/system/
+confirm_and_cp() {
+    local src=$1
+    local dest=$2
+    
+    read -p "cp $src $dest ? (y/n): " -n 1 -r
+    echo ""
+    
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        cp -i "$src" "$dest"
+    fi
+    echo "--------------------------------"
+}
+
+for file in $(find etc usr -type f 2>/dev/null); do
+    confirm_and_cp "$file" "/$file"
+done
+
