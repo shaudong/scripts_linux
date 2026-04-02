5b76e01ff07dcc395fb821c8e43ba91853265863
CHC <ch_chiang@arcadyan.com>
2026-04-03 02:04:43 +0800

    add etc/systemd/system/harbor.service

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 cp /usr/bin/docker_init usr/bin/
 cp /usr/bin/hudson_exec usr/bin/
 cp /etc/systemd/system/hudson-slave.service etc/systemd/system/
+cp /etc/systemd/system/harbor.service etc/systemd/system/
