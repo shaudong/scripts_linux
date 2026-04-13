8ccbc25ab92551d256e4ffd037992b9aadbca246
CHC <ch_chiang@arcadyan.com>
2026-04-13 03:31:34 +0000

    update addalluser.sh to use absolute path /root/scripts

--- addalluser.sh
+++ addalluser.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-#./newuser.sh ch_chiang A120008
-./newuser.sh william_yuan A160046
-./newuser.sh clare_chung A170071
-./newuser.sh leo_wang A180062
-./newuser.sh sky_lin A230043
-./newuser.sh yw_liu A200008
-./newuser.sh elp_lin A100016
-./newuser.sh gary_hsieh A220103
+/root/scripts/newuser.sh ch_chiang A120008
+/root/scripts/newuser.sh william_yuan A160046
+/root/scripts/newuser.sh clare_chung A170071
+/root/scripts/newuser.sh leo_wang A180062
+/root/scripts/newuser.sh sky_lin A230043
+/root/scripts/newuser.sh yw_liu A200008
+/root/scripts/newuser.sh elp_lin A100016
+/root/scripts/newuser.sh gary_hsieh A220103
