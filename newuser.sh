e2d26cad4acbef4de26ca10c361ae8e0abe4e083
CHC <ch_chiang@arcadyan.com>
2026-04-13 03:16:59 +0000

    refine newuser.sh to support restore old user uid and gid add restoreuser.sh for builder_omci6

--- newuser.sh
+++ newuser.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
+#
+# newuser.sh <username> <password> [uid] [gid]
+# create a new user with username, passwrd, uid and gid.
+# uid and gid is optional
 
 username=$1
 password="password"
 [ "$2" != "" ] && password=$2
+uid=$3
+gid=$4
 
-useradd -m -d /home/$username -s /bin/bash $username
+create_home="-m"
+[ -d /home/$username ] && create_home="-M"
+
+gid_opt=""
+if [ "$gid" != "" ]; then
+    groupadd -g $gid $username
+    gid_opt="-g $gid"
+fi
+
+uid_opt=""
+[ "$uid" != "" ] && uid_opt="-u $uid"
+
+useradd $create_home $uid_opt $gid_opt -d /home/$username -s /bin/bash $username
 echo -ne "$password\n$password\n" | passwd $username
 echo -ne "$password\n$password\n" | smbpasswd -a $username
 gpasswd -a $username docker
+
