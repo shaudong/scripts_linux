4c7a1e09a3030fafabe147e31a68cf37046e1f7e
CHC <ch_chiang@arcadyan.com>
2026-04-13 03:28:39 +0000

    backup /etc/group,gshadow,passwd,shadow /etc/samba/smb.conf add restoreuser.sh for reinstall system

---
+++ builder_omci5/restoreuser.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+/root/scripts/newuser.sh ch_chiang A120008 1001 1001
+/root/scripts/newuser.sh william_yuan A160046 1002 1002
+/root/scripts/newuser.sh clare_chung A170071 1003 1003
+/root/scripts/newuser.sh leo_wang A180062 1004 1004
+/root/scripts/newuser.sh sky_lin A230043 1005 1005
+/root/scripts/newuser.sh yw_liu A200008 1006 1006
+/root/scripts/newuser.sh elp_lin A100016 1007 1007
+/root/scripts/newuser.sh gary_hsieh A220103 1008 1008
+/root/scripts/newuser.sh hudson hudson 1009 1009
+
